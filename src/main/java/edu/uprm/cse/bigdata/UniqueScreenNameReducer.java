1a4223d45b2caa27212e26e4e0e856c5c650ab3f
alejandra <alejandra.casanova@upr.edu>
2018-10-14 19:01:40 -0400

    still not working good... :(

--- src/main/java/edu/uprm/cse/bigdata/UniqueScreenNameReducer.java
+++ src/main/java/edu/uprm/cse/bigdata/UniqueScreenNameReducer.java
@@ -1,5 +1,6 @@
 package edu.uprm.cse.bigdata;
 
+import com.google.common.collect.Iterables;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -9,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 import java.io.IOException;
 
 //This class will count all the occurrences of the key, which is the screen name.
-
+// THe algortihm is once the count of each screen_name is done, if screen_name has >1 then it is not unique, therefore it will be discarded.
 public class UniqueScreenNameReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 
     protected void reduce(Text key, Iterable<IntWritable> values, Reducer.Context context)
@@ -22,13 +23,14 @@ public class UniqueScreenNameReducer extends Reducer<Text,IntWritable,Text,IntWr
         for (IntWritable value : values ){
             count++;
         }
+        if(Iterables.size(values)<2){
+            context.write(key, new IntWritable(count));
+        }
         // emit key-pair: key, count
         // key is the word
         // count is the number of times that word appeared on the tweets
         Logger logger = LogManager.getRootLogger();
-        //logger.trace("Red: " + key.toString());
 
-        // DEBUG
-        context.write(key, new IntWritable(count));
+
     }
 }
